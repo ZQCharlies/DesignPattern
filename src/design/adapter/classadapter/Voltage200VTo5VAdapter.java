75e52cbc0f7c33842307dabae8238add5de8a5c6
wzq <zhengquwang@outlook.com>
2022-07-24 10:01:44 +0800

    classadapter

--- src/design/adapter/classadapter/Voltage200VTo5VAdapter.java
+++ src/design/adapter/classadapter/Voltage200VTo5VAdapter.java
@@ -3,7 +3,7 @@ package design.adapter.classadapter;
 /**
  * @Author: wzq
  * @Date: 2022/7/24
- * @Desc: 电压适配器（将220V电压适配5V电压）
+ * @Desc: 类适配器:电压适配器,将220V电压适配5V电压
  **/
 public class Voltage200VTo5VAdapter extends Voltage220V implements IVoltage5V{
 
